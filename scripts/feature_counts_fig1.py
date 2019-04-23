8b28e49f86723ba3da1103d0572280bb1736ec47
Zach Dwyer <zdwyer@gmail.com>
2019-04-23 11:42:07 -0400

    Add feature_counts_fig1.py

---
+++ scripts/feature_counts_fig1.py
@@ -0,0 +1,60 @@
+import sys, HTSeq, argparse
+from collections import defaultdict
+
+def main(args):
+ 
+	counts = {}
+
+	features = HTSeq.GenomicArrayOfSets("auto", stranded=False)
+	for line in open(args.feature_ranges):
+		fields = line.rstrip().split('\t')
+		iv = HTSeq.GenomicInterval(fields[0], int(fields[1])-1, int(fields[2]), fields[4])
+		features[iv] += fields[3]
+		counts[fields[3].split(';')[0]] = defaultdict(int)
+
+	for alignment in HTSeq.BAM_Reader(args.input_file):
+		
+		feat = set()
+		junctions = set()
+
+		for cigop in alignment.cigar:
+			if cigop.type == 'M':
+				for iv, val in features[cigop.ref_iv].steps():
+					feat |= val
+			elif cigop.type == 'N':
+				for iv, val in features[cigop.ref_iv].steps():
+					junctions |= val
+
+		candidate_genes = set()
+		for i in feat:
+			candidate_genes.add(i.split(';')[0])
+		for i in junctions:
+			candidate_genes.add(i.split(';')[0])
+
+		if len(candidate_genes) == 1:
+			if len(junctions) == 1:
+				counts[list(junctions)[0].split(';')[0]]['Mature'] += 1
+			else:
+				if len(feat) > 0:
+					gene = list(feat)[0].split(';')[0]
+					pieces = set()
+					for i in feat:
+						pieces.add(i.split(';')[1])
+					if 'I1' in pieces:
+						counts[gene]['Premature'] += 1
+					elif 'E1' in pieces or 'E2' in pieces:
+						counts[gene]['Exonic'] += 1
+
+	print 'Gene\tExonic\tMature\tPremature'
+	for gene in sorted(counts):
+		print '%s\t%d\t%d\t%d' % (gene, counts[gene]['Exonic'], counts[gene]['Mature'], counts[gene]['Premature'])
+
+def parseArguments():
+	parser = argparse.ArgumentParser(prog="", description='Counts the number of reads that are completely exonic, cross junctions, or cross an intron exon boundary.', usage='%(prog)s [options]')
+	required = parser.add_argument_group('required arguments')
+	required.add_argument('-i', '--input_file', required=True, help=' Input alignment file. (.bam)', metavar='', dest='input_file')
+	required.add_argument('-f', '--features', required=True, help=' Ranges of all exons and introns to be counted. (.bed)', metavar='', dest='feature_ranges')
+	return parser.parse_args()
+
+args = parseArguments()
+main(args)
