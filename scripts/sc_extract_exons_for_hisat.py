dfdbdede9b8e2628e7a77fba451ac4fdcea7e7c7
Zach Dwyer <zdwyer@gmail.com>
2019-04-23 11:47:12 -0400

    Update sc_extract_introns_for_hisat.py and sc_extract_introns_for_hisat.py

--- scripts/sc_extract_exons_for_hisat.py
+++ scripts/sc_extract_exons_for_hisat.py
@@ -1,7 +1,18 @@
-import sys
+import argparse
 
-for line in open(sys.argv[1]):
-	if line[0] != '#':
-		cur = line.rstrip().split('\t')
-		if cur[2] == 'CDS':
-			print '%s\t%d\t%d' % (cur[0], int(cur[3])-1, int(cur[4])-1)
+def main(args):
+
+	for line in open(args.feature_file):
+		if line[0] != '#':
+			cur = line.rstrip().split('\t')
+			if cur[2] == 'CDS':
+				print '%s\t%d\t%d' % (cur[0], int(cur[3])-1, int(cur[4])-1)
+
+def parseArguments():
+	parser = argparse.ArgumentParser(prog="sc_extract_exons_for_hisat.py", description='Extract the exon ranges from a feature file in a format suitable for hisat', usage='%(prog)s -f <feature file>.gff')
+	required = parser.add_argument_group('required arguments')
+	required.add_argument('-f', '--feature_file', required=True, help=' Feature file', metavar='', dest='feature_file')
+	return parser.parse_args()
+
+args = parseArguments()
+main(args)
--- scripts/sc_extract_introns_for_hisat.py
+++ scripts/sc_extract_introns_for_hisat.py
@@ -1,7 +1,18 @@
 import sys
 
-for line in open(sys.argv[1]):
-	if line[0] != '#':
-		cur = line.rstrip().split('\t')
-		if cur[2] == 'intron':
-			print '%s\t%d\t%d\t%s' % (cur[0], int(cur[3])-1, int(cur[4])-1, cur[6])
+def main(args):
+
+	for line in open(sys.argv[1]):
+		if line[0] != '#':
+			cur = line.rstrip().split('\t')
+			if cur[2] == 'intron':
+				print '%s\t%d\t%d\t%s' % (cur[0], int(cur[3])-1, int(cur[4])-1, cur[6])
+
+def parseArguments():
+	parser = argparse.ArgumentParser(prog="sc_extract_introns_for_hisat.py", description='Extract the intron ranges from a feature file in a format suitable for hisat', usage='%(prog)s -f <feature file>.gff')
+	required = parser.add_argument_group('required arguments')
+	required.add_argument('-f', '--feature_file', required=True, help=' Feature file', metavar='', dest='feature_file')
+	return parser.parse_args()
+
+args = parseArguments()
+main(args)
